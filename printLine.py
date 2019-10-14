77b61eda23299d2b9efbebc2234b549d392dfff4
Colin <cstatro@gmail.com>
2019-10-13 22:27:07 -0400

    added format method

--- printLine.py
+++ printLine.py
@@ -4,5 +4,8 @@ print("hello python")
 x = "hello there"
 y = "bob"
 
-# F-strings is how we interpolate in Python
+# F-strings is how we interpolate in Python 3.6+
 print(f"{x} {y}")
+
+# Old way Python 3.5 and below
+print("{} {}".format(x, y))
